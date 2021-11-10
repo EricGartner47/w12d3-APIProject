4744d7be6c9325b03dd7e867bf6c000527edb8fb
Your Name <email@example.com>
2021-11-10 16:00:10 -0500

    phase three

---
+++ routes/tweets.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../db/models");
+
+const  {Tweet} = db;
+
+const asyncHandler = (handler) => {
+    return (req, res, next) => {
+        return handler(req, res, next).catch(next);
+    };
+};
+
+
+// router.get("/", (req, res) => {
+//     res.json({message: "test tweets index"});
+// });
+
+router.get("/", asyncHandler(async(req, res)=> {
+    const tweets = await Tweet.findAll()
+    res.json({tweets})
+}))
+
+router.get("/:id(\\d+)", asyncHandler(async(req, res, next)=> {
+    const tweetId = await Tweet.findByPk(req.params.id)
+    console.log(tweetId.message)
+    res.json({tweetId})
+}))
+
+module.exports = router
